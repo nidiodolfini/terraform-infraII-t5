bfaa8fdc22e7b62b9050457c205f3c1ebb74b802
Nídio Dolfini <nidiosdolfini@gmail.com>
2022-10-31 20:20:07 -0300

    ansible

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "nidio_ec2" {
-  count         = 1
+  count         = 2
   ami           = var.ami
   instance_type = var.tipo_instancia
   key_name      = "${var.usuario}-terraform-aws"
@@ -7,22 +7,22 @@ resource "aws_instance" "nidio_ec2" {
     "Name" = "${var.usuario}-ec2-${count.index}"
   }
   vpc_security_group_ids = [ aws_security_group.sg_acesso_ssh_publico.id, aws_security_group.sg_acesso_web.id ]
+}
 
+resource "aws_instance" "nidio_ec2_ansible" {
+  count         = 1
+  ami           = var.ami
+  instance_type = var.tipo_instancia
+  key_name      = "${var.usuario}-terraform-aws"
+  tags = {
+    "Name" = "${var.usuario}-ec2-ansible-${count.index}"
+  }
+  vpc_security_group_ids = [ aws_security_group.sg_acesso_ssh_publico.id, aws_security_group.sg_acesso_web.id ]
   user_data = <<-EOF
     #!/bin/bash
     sudo apt-get update
-    sudo apt-get install -y \
-    ca-certificates -y \
-    curl -y \
-    gnupg -y \
-    lsb-release -y
-    sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-        
-    EOF
+    sudo apt-get install software-properties-common -y
+    sudo add-apt-repository --yes --update ppa:ansible/ansible -y
+    sudo apt-get install ansible -y
+  EOF
 }
--- output.tf
+++ output.tf
@@ -4,3 +4,7 @@ output "instancias_ec2" {
 output "instancias_ec2_private" {
   value = aws_instance.nidio_ec2.*.private_ip
 }
+
+output "instancias_ec2_ansible" {
+  value = aws_instance.nidio_ec2_ansible.*.public_ip
+}
